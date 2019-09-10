26c22c9371e9b163566ee8f83ac4af68cac3a10c
Theodore Konstantopoulos <konstantopoulostheodore@gmail.com>
2019-09-10 16:27:45 -0400

    Initial commit

---
+++ app/src/main/java/tkonstantopoulos/flagquiz/MainActivity.java
@@ -0,0 +1,35 @@
+package tkonstantopoulos.flagquiz;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_name) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
